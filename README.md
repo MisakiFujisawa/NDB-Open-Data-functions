9e649257fd7355bbe36bb484a18a9ddad42412e4
Misaki Fujisawa <mskfjsw@MisakinoMacBook-Pro.local>
2023-11-30 14:01:07 +0900

    scr_cal()の追加

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ndb-scr-calculation
-NDBオープンデータの分析に役立つ関数を公開しています。
+# 国立長寿医療研究センター医療経済研究部のGithubアカウントです。NDBオープンデータの分析に役立つ関数を公開しています。
+
+### scr_cal()　
+
+NDBオープンデータと人口動態統計からSCRを計算するための関数です。以下の4つのデータフレームを渡し、各都道府県別のSCRが出力されます。　　
+
+1.  sinryou_agesex　3列 age, sex, sinryou
+
+2.  sinryou_pref　2列 sinryou, prefname
+
+3.  population_agesex　3列 age, sex, population
+
+4.  population_pref　4列 prefname, age, sex, population
---
+++ functions/scr_cal.R
@@ -0,0 +1,46 @@
+#計算に用いるデータセット概要
+#sinryou_agesex　3列 age, sex, sinryou
+#sinryou_pref　2列 sinryou, prefname
+#population_agesex　3列 age, sex, population
+#population_pref　4列 prefname, age, sex, population
+
+#計算式　標準化レセプト出現比 SCR = 都道府県別レセプト数/期待レセプト数　　　　　
+#.       期待レセプト数 = Σ（都道府県別5際階級性別人口✖️基準レセプト数）
+#.       基準レセプト数 = 日本全体の5歳階級性別レセプト数/日本全体の5歳階級性別人口
+
+library(tidyverse)
+scr_cal <- function(sinryou_agesex, sinryou_pref, population_agesex, population_pref){
+  
+  # Check if sinryou_agesex is empty
+  if (nrow(sinryou_agesex) == 0||nrow(sinryou_pref)==0) {
+    warning("sinryou_agesex is empty. Function will return NULL.")
+    return(NULL)
+  }
+  
+  #基準レセプト請求数 
+  scr <- sinryou_agesex %>% 
+    full_join(population_agesex) %>% 
+    mutate(standard_rece=sinryou/(population*1000)) %>%  
+    select(age,sex,standard_rece) %>% ungroup()
+  
+  #期待レセプト請求数
+  scr1 <- population_pref %>% left_join(scr) %>% 
+    mutate(expect_rece=population*1000*standard_rece) %>% 
+    select(prefname, expect_rece) %>% 
+    group_by(prefname) %>% 
+    summarise(across(everything(),sum)) %>% 
+    mutate(prefname=str_trim(prefname,side="both")) %>% 
+    ungroup()
+  
+  scr2 <- sinryou_pref %>% full_join(scr1) %>% 
+    mutate(scr=sinryou*100/expect_rece) %>% 
+    select(prefname,scr)
+  
+  return(scr2)
+ 
+}
+
+
+
+
+
